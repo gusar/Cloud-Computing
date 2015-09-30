6305cf0f63b6beb66b499198b4be2d04b245ab37
gusar <andy.lahs@gmail.com>
2015-09-30 15:59:46 +0000

    euler19 working

--- lab2/euler19.py
+++ lab2/euler19.py
@@ -2,34 +2,43 @@ yearCount = 0
 year = 1900
 month = 0
 week = 0
-leap = false
+leap = False
 dayrange = 6
 sundayCount = 0
 weekDay = 0
 
+# count years
 for year in range(1900, 1999):
-  count += 1
-  if count % 4 == 0 and year % 400 != 0:
-    leap true
-  else
-    leap false
+  yearCount += 1
+  if yearCount % 4 == 0 and year % 400 != 0:
+    leap = True
+  else:
+    leap = False
+
+  # count months
   for month in range (0, 11):
+
+    # count weeks
     for week in range (0, 4):
       if week == 4:
         if month == 3 or month == 5 or month == 8 or month == 11:
           dayrange = 2
-        else if month == 1:
-          if leap == false:
+        elif month == 1:
+          if leap == False:
             dayrange = 0
           else:
             dayrange = 1
         else:
           dayrange = 3
+
+      # count days
       for day in range (0, dayrange):
         if weekDay == 7:
           sundayCount += 1
           weekDay = 0
-        weekDay +1
+        else:
+          weekDay += 1
 
-print(sundayCount)
+# print result
+print("\nSunday count in 18th century: ", sundayCount)
 
