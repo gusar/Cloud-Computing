f989dcbb751bcd20d0bc6dda56725655131b0a48
gusar <andy.lahs@gmail.com>
2015-09-30 15:28:41 +0000

    add euler25

--- lab2/eluer19.py
+++ lab2/eluer19.py
@@ -3,6 +3,8 @@ year = 1900
 month = 0
 week = 0
 leap = false
+dayrange = 6
+
 for year in range(1900, 1999):
   count += 1
   if count % 4 == 0 and year % 400 != 0:
@@ -11,4 +13,7 @@ for year in range(1900, 1999):
     leap false
   for month in range (0, 11):
     for week in range (0, 3):
-      for day in range (0, 6):
+      if month == 1 and leap == true:
+        dayrange =
+      for day in range (0, dayrange):
+
