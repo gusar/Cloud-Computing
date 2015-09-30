303db311c1483061513a54fa71346bbc1c5a6c6e
gusar <andy.lahs@gmail.com>
2015-09-30 16:03:12 +0000

    euler19 add comments

--- lab2/euler19.py
+++ lab2/euler19.py
@@ -10,6 +10,7 @@ weekDay = 0
 # count years
 for year in range(1900, 1999):
   yearCount += 1
+  # mark leap years
   if yearCount % 4 == 0 and year % 400 != 0:
     leap = True
   else:
@@ -20,19 +21,24 @@ for year in range(1900, 1999):
 
     # count weeks
     for week in range (0, 4):
+      # last week of the month adjustment
       if week == 4:
+        # 30-day months
         if month == 3 or month == 5 or month == 8 or month == 11:
           dayrange = 2
+        # february case
         elif month == 1:
           if leap == False:
             dayrange = 0
           else:
             dayrange = 1
+        # 31-day months
         else:
           dayrange = 3
 
       # count days
       for day in range (0, dayrange):
+        # reset week each 7 days
         if weekDay == 7:
           sundayCount += 1
           weekDay = 0
