6aa53240ab2f99141fc83dec37b46766e0b1b476
gusar <andy.lahs@gmail.com>
2015-09-30 15:47:31 +0000

    euler19 checkin

--- lab2/euler19.py
+++ lab2/euler19.py
@@ -4,6 +4,8 @@ month = 0
 week = 0
 leap = false
 dayrange = 6
+sundayCount = 0
+weekDay = 0
 
 for year in range(1900, 1999):
   count += 1
@@ -12,8 +14,22 @@ for year in range(1900, 1999):
   else
     leap false
   for month in range (0, 11):
-    for week in range (0, 3):
-      if month == 1 and leap == true:
-        dayrange =
+    for week in range (0, 4):
+      if week == 4:
+        if month == 3 or month == 5 or month == 8 or month == 11:
+          dayrange = 2
+        else if month == 1:
+          if leap == false:
+            dayrange = 0
+          else:
+            dayrange = 1
+        else:
+          dayrange = 3
       for day in range (0, dayrange):
+        if weekDay == 7:
+          sundayCount += 1
+          weekDay = 0
+        weekDay +1
+
+print(sundayCount)
 
