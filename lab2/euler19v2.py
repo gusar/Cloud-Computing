19a4cfc993016108a0cc338f2700d0a8298fe204
gusar <andy.lahs@gmail.com>
2015-09-30 20:28:05 +0000

    euler19 v2 working

--- lab2/euler19v2.py
+++ lab2/euler19v2.py
@@ -5,3 +5,5 @@ for year in range(1900, 2000):
   for month in range(1, 13):
     if calendar.weekday(year, month, 1) == 1:
       sundayCount += 1
+
+print("The number of Sundays on the first day of the month in the 20th century is: ", sundayCount)
